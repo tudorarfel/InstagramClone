ecd3031e68a680e653caad5006f076b6c370abeb
tudorarfel <tudor.arfel@gmail.com>
2020-02-12 14:57:18 +0200

    SignUp and Loin Parse

--- app/src/main/java/com/udemy/instagramclone/SignUp.java
+++ app/src/main/java/com/udemy/instagramclone/SignUp.java
@@ -2,6 +2,7 @@ package com.udemy.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -67,7 +68,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
             }
         });*/
 
-        btnGetAllData.setOnClickListener(new View.OnClickListener() {
+       /* btnGetAllData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -104,12 +105,14 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                     }
                 });
             }
-        });
+        });*/
 
         btnTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(SignUp.this, SignUpLoginActivity.class);
+                startActivity(intent);
 
             }
         });
