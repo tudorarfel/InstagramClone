e76a900a2ce8061e7f70c887528622b15ca97192
tudorarfel <tudor.arfel@gmail.com>
2020-02-14 11:23:48 +0200

    Implemented the Users Tab

--- app/src/main/java/com/udemy/instagramclone/ProfileTab.java
+++ app/src/main/java/com/udemy/instagramclone/ProfileTab.java
@@ -8,12 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+
+    private EditText edtProfileName, edtProfileBio, edtProfileProfession, edtProfileHobbies, edtProfileFavSport;
+    private Button btnUpdateInfo;
 
 
     public ProfileTab() {
@@ -25,7 +36,89 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileFavSport = view.findViewById(R.id.edtProfileFavouriteSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if (parseUser.get("profileName") == null) {
+            edtProfileName.setText("");
+
+        } else {
+
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+
+        if (parseUser.get("profileBio") == null) {
+            edtProfileName.setText("");
+
+        } else {
+
+            edtProfileBio.setText(parseUser.get("profileBio").toString());
+        }
+
+        if (parseUser.get("profileProfession") == null) {
+            edtProfileName.setText("");
+
+        } else {
+
+            edtProfileProfession.setText(parseUser.get("profileProfession").toString());
+        }
+
+        if (parseUser.get("profileHobbies") == null) {
+            edtProfileName.setText("");
+
+        } else {
+
+            edtProfileHobbies.setText(parseUser.get("profileHobbies").toString());
+        }
+
+        if (parseUser.get("profileFavSport") == null) {
+            edtProfileName.setText("");
+
+        } else {
+
+            edtProfileFavSport.setText(parseUser.get("profileFavSport").toString());
+        }
+
+
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName", edtProfileName.getText().toString());
+                parseUser.put("profileBio", edtProfileBio.getText().toString());
+                parseUser.put("profileProfession", edtProfileProfession.getText().toString());
+                parseUser.put("profileHobbies", edtProfileHobbies.getText().toString());
+                parseUser.put("profileFavSport", edtProfileFavSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+
+                            FancyToast.makeText(getContext(), "Info Updated",
+                                    Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
+                        } else {
+
+                            FancyToast.makeText(getContext(), e.getMessage(),
+                                    Toast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 
 }
